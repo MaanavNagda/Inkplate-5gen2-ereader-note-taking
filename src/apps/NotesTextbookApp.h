7ea18e26218a3d312732e963eb9c19acb1a62367
Maanav <maanavnagda2197@gmail.com>
2026-09-03 22:57:29 +0100

    Make NotesTextbookApp inherit TextbookApp for identical page selector

--- src/apps/NotesTextbookApp.h
+++ src/apps/NotesTextbookApp.h
@@ -1,18 +1,12 @@
 #ifndef NOTES_TEXTBOOK_APP_H
 #define NOTES_TEXTBOOK_APP_H
 
-#include "../App.h"
+#include "TextbookApp.h"
 
-class NotesTextbookApp : public App {
+class NotesTextbookApp : public TextbookApp {
 public:
     AppId id() const override { return AppId::NOTES_TEXTBOOK; }
     const char* name() const override { return "Notes + Textbook"; }
-    void begin(AppManager& manager) override;
-    void update(uint32_t dtMs) override;
-    void render(Inkplate& display) override;
-    void onButton(ButtonAction action) override;
-private:
-    bool needsRender_ = true;
 };
 
 #endif
